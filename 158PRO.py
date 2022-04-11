7482165b77739a74ee2dda4cc497f4649ae6a1b5
Git-nirmay <79860151+Git-nirmay@users.noreply.github.com>
2022-04-12 00:02:32 +0530

    Add files via upload

---
+++ 158PRO.py
@@ -0,0 +1,18 @@
+from tkinter import *
+root=Tk()
+root.title("Addition")
+root.geometry("600x400")
+input_box = Entry(root)
+input_box.pack()
+
+def addition():
+    number = 17
+    get_input = input_box.get()
+    try:
+        print(number + get_input)
+    except(TypeError):
+	    messagebox.showinfo("Error", "Cannot add two different data types: integer and string") 
+    
+button = Button(root , text= "addition", command = addition)
+button.pack()
+root.mainloop()
